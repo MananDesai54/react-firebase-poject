c7820f910be72f7aa88fa7a16c2ba0925c013b34
Manan Desai <52067783+MananDesai54@users.noreply.github.com>
2020-06-14 23:19:31 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,1 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
 //Your fire base connection code
+var firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDERID",
+    appId: "YOUR_APPID",
+    measurementId: "YOUR_MESUREMENTID"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore();
+
+export default firebase;
